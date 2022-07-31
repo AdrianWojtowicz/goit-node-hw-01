0bac29fd6944dea411074e1dc2bb8a1262b3e7e8
AdrianWojtowicz <adrian.wojtowicz@op.pl>
2022-07-31 17:48:50 +0200

    .

---
+++ README.md
@@ -0,0 +1,2 @@
+All screenshots:
+https://www.icloud.com/iclouddrive/087yGumlTmuSb0-VUEelFWnPQ#goit-node-hw-01
---
+++ index.js
@@ -0,0 +1,34 @@
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv
+
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const requestedContact = await contacts.getContactById(id);
+      console.log(requestedContact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
